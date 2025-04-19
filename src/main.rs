8ef7909d14085ddc2e036480963716ab6f1e0766
green-ogre <109698029+void-scape@users.noreply.github.com>
2025-04-19 01:58:11 -0600

    ready to use

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Update, close_on_escape)
-        .add_systems(Startup, startup)
         .run();
-}
-
-fn startup(mut commands: Commands, server: Res<AssetServer>) {
-    commands.spawn(Camera2d::default());
-    commands.spawn(Sprite::from_image(server.load("clouds.png")));
 }
 
 fn close_on_escape(mut input: EventReader<KeyboardInput>, mut writer: EventWriter<AppExit>) {
